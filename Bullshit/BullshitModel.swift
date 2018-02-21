aca1a9d80f0c2129bf7f0cec21cc977391ecca4d
L.A. van de Guchte <s2408287@ai178142.ai.rug.nl>
2018-02-21 12:55:02 +0100

    test

---
+++ Bullshit/BullshitModel.swift
@@ -0,0 +1,10 @@
+//
+//  BullshitModel.swift
+//  Bullshit
+//
+//  Created by L.A. van de Guchte on 21/02/2018.
+//  Copyright © 2018 L.A. van de Guchte. All rights reserved.
+//
+
+import Foundation
+
