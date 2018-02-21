7430d9fc95a2204184e71d8fd19cc9c5179e1e22
L.A. van de Guchte <s2408287@usabilitylabs-imac-6.local>
2018-02-21 12:49:52 +0100

    Initial Commit

---
+++ Bullshit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bullshit
+//
+//  Created by L.A. van de Guchte on 21/02/2018.
+//  Copyright © 2018 L.A. van de Guchte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
