e2a95e70f6025ca6b24bb68626c864ce1b3798e3
lisadeckers <lisa.m.deckers@gmail.com>
2018-03-16 10:59:17 +0100

    claim pop up (open and close) without drop down menu

--- Bullshit/claimPopUpViewController.swift
+++ Bullshit/claimPopUpViewController.swift
@@ -12,6 +12,10 @@ class claimPopUpViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.black.withAlphaComponent(0.8)
+        
+        self.showAnimate()
 
         // Do any additional setup after loading the view.
     }
@@ -21,7 +25,33 @@ class claimPopUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func cancelClaimPopUp(_ sender: UIButton) {
+        self.removeAnimate()
+        //self.view.removeFromSuperview()
+    }
+    
+    func showAnimate(){
+        self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        self.view.alpha = 0.0;
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.alpha = 1.0
+            self.view.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        });
+    }
+    
+    func removeAnimate(){
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+            self.view.alpha = 0.0;
+        }, completion:{(finished : Bool)  in
+            if (finished)
+            {
+                self.view.removeFromSuperview()
+            }
+        });
+    }
 
+    
     /*
     // MARK: - Navigation
 
